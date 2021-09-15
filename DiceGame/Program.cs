b901edc5faf2b2e2254cdbf5cefe1ef0bf84ace9
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:52:39 +0300

    RNG

---
+++ DiceGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mängus on kas magijat
+            //Mõlemad mängijad viskavad täringut
+            //Programm kontrollib, kumb mängija viskasa rohkem
+            //Mängija, kes viskas rohkem on võiteja
+            //Program kuulutab võitat
+            Random rnd = new Random();            
+            string Playerone = "Ted";
+            string Playertwo = "Bob";
+            int userThrow = rnd.Next(1, 7);
+            int BobThrow = rnd.Next(1, 7);            
+            Console.WriteLine($"Hello {Playerone} please press [enter] to roll your dice.");
+            Console.ReadLine();
+            Console.WriteLine($"{Playerone} rolled {userThrow}");
+            Console.WriteLine($"Plese press [enter] again to roll for {Playertwo}.");
+            Console.ReadLine();
+            Console.WriteLine($"{Playertwo} rolled {BobThrow}.");
+            if (userThrow > BobThrow)
+            {
+                Console.WriteLine($"{Playerone} wins! ");
+            }
+            else if  (userThrow < BobThrow)
+            {
+                Console.WriteLine($"{Playertwo} wins!");
+            }
+            else
+            {
+                Console.WriteLine("No one wins it's a draw!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int RNG = rnd.Next(1, 11);//Number 1 kuni 10.
+            Console.WriteLine($"Your Random number is {RNG}");
+
+            //Programm kontollib, kas genereeritud number on suurem kui 5
+            //kui genereeritud number on suurem kui 5, program kuvab
+            //"juhulik number on {RNG}, see on suurem kui viis"
+            //kui on väiksem kui viis siis:
+            //"juhulik number on {RNG}, see on väiksem kui 5."
+            // ja kui number on viis siis:
+            // "juhulsik number on 5."
+            //(< - väiksem kui) , (> - suurem kui) , (== - võrdne)
+            if (RNG > 5)
+            {
+                Console.WriteLine($"Your Randomly generated number is {RNG}, It's bigger then 5.");
+            }
+            else if (RNG < 5)
+            {
+                Console.WriteLine($"Your Randomly generated number is {RNG}, It's smaler than 5.");
+            }
+            else
+            {
+                Console.WriteLine($"Your Randomly generated number is {RNG}."); 
+            }
+            Console.ReadLine();
+            Console.WriteLine("Have a Nice day.");
+        }
+    }
+}
